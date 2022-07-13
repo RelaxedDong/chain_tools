dbe2ce3dfa203a9ab36d343b4e5b751792e42469
donghao <hao.dong@bca.network>
2022-07-13 13:36:25 +0800

    fix: add env file

--- bin/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-# @Time    : 2022/7/11 4:28 PM
-# @Author  : donghao
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -2,12 +2,10 @@
 # -*- coding:utf-8 -*-
 # @Time    : 2022/6/18 2:03 PM
 # @Author  : donghao
-
-from bin.main import app
+from main import app
 
 
 def test_basic_test_client():
-    request, response = app.test_client.get("/")
-    response_json = response.json
-    assert response_json['version'] == 2
+    request, response = app.test_client.get("/monitor/new_east_mints")
+    assert 'records' in response.json
     assert response.status == 200
